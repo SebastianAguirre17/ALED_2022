ae299f41437649e2fed1f812c25562528db6a373
SebastianAguirre17 <sebaaguirre2012@gmail.com>
2022-04-22 14:32:18 -0300

    Iniciando tp 3

---
+++ Trabajo Práctico 3/AGUIRRE_SEBASTIAN_ALUMNO_EJ1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+
+#include "../AGUIRRE_SEBASTIAN_BIBLIOTECA.h" 
+
+#define TOPE 20
+
+int main(){
+    mostrarTitulo("Ejercicio 1");
+
+	return EXIT_SUCCESS;
+}
